76d77b8b63360cdd0858a5958e46b71222a260a5
YeganaAbd <abd.yegane@gmail.com>
2021-12-28 01:26:54 +0000

    JS

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 let board = document.querySelector("#board");
 let allCells = document.querySelectorAll(".cell");
+let restartBtn = document.querySelector("#restart");
+allCells.forEach((cell) => cell.addEventListener("click", handleClick));
+
+function handleClick(e) {
+  let clickedCell = e.target;
+}

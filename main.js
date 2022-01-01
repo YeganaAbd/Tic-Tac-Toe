2adacde9f54da2789d5bfe38553691caa074316c
YeganaAbd <abd.yegane@gmail.com>
2022-01-01 01:07:14 +0000

    Restart

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let x_class = "x";
 let o_class = "circle";
-let oTurn = "";
+let oTurn = false;
 let allCells = document.querySelectorAll(".cell");
 let board = document.querySelector("#board");
 let restartBtn = document.querySelector("#restart");
@@ -18,13 +18,17 @@ const winner_result = [
 ];
 let arrX = [];
 let arrO = [];
-
+let hasFinished = false;
 allCells.forEach((cell) =>
   cell.addEventListener("click", handleClick, { once: true })
 );
 
 function handleClick(e) {
+  if (hasFinished) {
+    return;
+  }
   let currentClick = e.target;
+
   // console.log(currentClick);
   if (oTurn) {
     currentClass = o_class;
@@ -33,8 +37,10 @@ function handleClick(e) {
   }
   clickedClass(currentClick, currentClass);
   changeTurn();
-  checkWinner();
-  checkResult();
+  hasFinished = checkResult();
+  if (hasFinished) {
+    console.log("Array X : " + arrX);
+  }
 }
 
 function clickedClass(currentClick, currentClass) {
@@ -46,32 +52,58 @@ function changeTurn() {
   oTurn = !oTurn;
 }
 
-function checkWinner() {
-  for (let i = 0; i < allCells.length; i++) {
-    if (allCells[i].classList.contains(o_class)) {
-      arrO.push(i);
-    }
-    if (allCells[i].classList.contains(x_class)) {
-      arrX.push(i);
-    }
-  }
-}
+// function collectClasses() {
+//   for (let i = 0; i < allCells.length; i++) {
+//     if (allCells[i].classList.contains(o_class)) {
+//       arrO.push(i);
+//     }
+//     if (allCells[i].classList.contains(x_class)) {
+//       arrX.push(i);
+//     }
+//   }
+// }
 
-function checkResult(draw) {
-  if (draw) {
-    winnerMsg.innerText = `Draw`;
-  }
+function checkResult() {
   for (let i = 0; i < winner_result.length; i++) {
-    if (winner_result[i].every((item) => arrX.includes(item))) {
+    if (
+      winner_result[i].every((item) =>
+        allCells[item].classList.contains(x_class)
+      )
+    ) {
       winnerMsg.innerText = `X won`;
+      return true;
     }
-    if (winner_result[i].every((item) => arrO.includes(item))) {
+    if (
+      winner_result[i].every((item) =>
+        allCells[item].classList.contains(o_class)
+      )
+    ) {
       winnerMsg.innerText = `O won`;
+      return true;
     }
   }
+  if (checkDraw()) {
+    winnerMsg.innerText = `Draw`;
+    return true;
+  }
+  return false;
 }
 function checkDraw() {
   return [...allCells].every((item) => {
     return item.classList.contains(x_class) || item.classList.contains(o_class);
   });
+}
+
+restartBtn.addEventListener("click", newGame);
+
+function newGame() {
+  allCells.forEach((cell) => {
+    cell.classList.remove(x_class);
+    cell.classList.remove(o_class);
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  hasFinished = false;
+  winnerMsg.innerText = "";
+  oTurn = false;
 }
